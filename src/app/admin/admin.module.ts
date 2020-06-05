71321dd1ceda1615c1876f234346802d3f0bd5ec
Christian Acevedo <christian_acevedo@hotmail.com>
2020-06-04 22:52:17 -0500

    formulario de edicion de producto

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -11,9 +11,10 @@ import { TableComponent } from './components/table/table.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { MaterialModule } from './../material/material.module';
 import { FormProductComponent } from './components/form-product/form-product.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
 
 @NgModule({
-  declarations: [ProductFormComponent, NavComponent, DashboardComponent, TableComponent, ProductsListComponent, FormProductComponent],
+  declarations: [ProductFormComponent, NavComponent, DashboardComponent, TableComponent, ProductsListComponent, FormProductComponent, ProductEditComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
--- src/app/admin/components/product-edit/product-edit.component.ts
+++ src/app/admin/components/product-edit/product-edit.component.ts
@@ -1,45 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { ProductsService } from './../../../core/services/products/products.service';
 import { MyValidators } from './../../../utils/validators';
 
 @Component({
-  selector: 'app-form-product',
-  templateUrl: './form-product.component.html',
-  styleUrls: ['./form-product.component.scss']
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.scss']
 })
-export class FormProductComponent implements OnInit {
+export class ProductEditComponent implements OnInit {
 
   form: FormGroup;
+  id: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
   ) {
     this.buildForm();
   }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      const id = params.id;
+      this.productsService.getProduct(id)
+        .subscribe(product => {
+          this.form.patchValue(product);
+        });
+    });
   }
 
   saveProduct(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
-      this.productsService.createProduct(product)
-      .subscribe((newProduct) => {
-        console.log(newProduct);
-        this.router.navigate(['./admin/products']);
-      });
+      this.productsService.updateProduct(this.id, product)
+        .subscribe((newProduct) => {
+          console.log(newProduct);
+          this.router.navigate(['./admin/products']);
+        });
     }
+
   }
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      id: ['', [Validators.required]],
       title: ['', [Validators.required]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
       image: [''],
